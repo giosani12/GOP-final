dc6660c0ad914c691b19643b6ef3f531fe08bb64
Frafilips <38458715+Frafilips@users.noreply.github.com>
2018-05-07 14:23:22 +0200

    Probabilità carte aggiunta

--- GOP/CARD.cpp
+++ GOP/CARD.cpp
@@ -10,7 +10,8 @@ CARD::CARD() {
 
 int CARD::randomCard()//Restituisce un numero casuale da 0 a 6, usata per riempire la struttura mazzo
 {
+	int tmp[25] = { 0,0,0,0,1,1,2,2,2,2,3,3,4,5,5,5,6,6,6,6,6,7,7,7,7 };
 	int value;
-	value = rand() % 8;
+	value = tmp[rand() % 25];
 	return value;
 }
